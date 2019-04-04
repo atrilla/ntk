493c4854505b13e503496ce6cc2b974f7b20cc1f
atrilla <alex@atrilla.net>
2019-04-04 19:41:43 +0200

    prnn init.

---
+++ explore/test_prnn.py
@@ -0,0 +1,71 @@
+# -----------------------------------------------------------------------
+# File    : test_prnn.py
+# Created : 04-Apr-2019
+# By      : Alexandre Trilla <alex@atrilla.net>
+#
+# NTK - Neural Network Toolkit
+#
+# Copyright (C) 2019 Alexandre Trilla
+# -----------------------------------------------------------------------
+#
+# This file is part of NTK.
+#
+# NTK is free software: you can redistribute it and/or modify it under
+# the terms of the MIT/X11 License as published by the Massachusetts
+# Institute of Technology. See the MIT/X11 License for more details.
+#
+# You should have received a copy of the MIT/X11 License along with
+# this source code distribution of NTK (see the COPYING file in the
+# root directory).
+# If not, see <http://www.opensource.org/licenses/mit-license>.
+#
+# -----------------------------------------------------------------------
+
+
+import NeuralNetwork
+import numpy as np
+import matplotlib.pyplot as plt
+
+# model exp decay
+def genseq_decay():
+    t = np.array(range(50 + int(50*np.random.rand())))
+    A = 0.5 + 5.0*np.random.rand()
+    alpha = -0.01 - 0.1*np.random.rand()
+    x = A*np.e**(alpha*t)
+    x = x.reshape(len(x),1)
+    return x
+
+# autoregressive predictor, next sample
+# Train
+x = []
+y = []
+for n in xrange(500):
+    aux = genseq_decay()
+    x.append(aux[:-1])
+    y.append(aux[1:])
+
+nn = NeuralNetwork.ELM([1,3,1])
+NeuralNetwork.PR_Backprop(nn, x, y, 0.1, 20, 0.01, 
+    af=NeuralNetwork.Sigmoid, c='sqerr', arch="ELM")
+
+test = genseq_decay()
+tx = test[:-1]
+ty = test[1:]
+
+err = []
+context = np.zeros(nn[0].shape[0])
+for n in xrange(len(tx)):
+    o = PR_Predict(nn, tx[n], context, af=NeuralNetwork.Sigmoid)
+    context = ELM_Context(o)
+    err.append(ty[n][0] - o[-1][0])
+
+plt.figure()
+plt.plot(err, 'b', label="Error")
+plt.xlabel("Time")
+plt.ylabel("Value")
+plt.legend(loc = "upper right")
+plt.title("Elman net")
+
+plt.show()
+
+
