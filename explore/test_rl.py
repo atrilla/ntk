a2e0297f3be2a6261de6f5bf3ac23b562516e6ce
atrilla <alex@atrilla.net>
2019-04-15 19:45:26 +0200

    rl pid floss dance.

--- explore/test_rl.py
+++ explore/test_rl.py
@@ -33,50 +33,35 @@ import numpy as np
 import gym
 import matplotlib.pyplot as plt
 
-# maintain pole vertically
-def _critic(info):
-    lmax = 0.5
-    vmax = 0.1
-    amax = 0.01
-    phi = info[0]
-    dphi = info[1] - info[0]
-    aux = info[3] - info[2]
-    ddphi = aux - dphi
-    loc = np.max([(lmax**2 - phi**2)/lmax**2, 0.0])
-    vel = np.max([(vmax**2 - dphi**2)/vmax**2, 0.0])
-    acc = np.max([(amax**2 - ddphi**2)/amax**2, 0.0])
-    return (0.2*loc + 0.3*vel + 0.5*acc)
-
 # acceleration...
-def _critic(info):
-    phi = info[0]
-    dphi = info[1] - info[0]
-    aux = info[3] - info[2]
-    ddphi = aux - dphi
-    return np.max([0.0, 1.0 - 1.0*phi**2 - 5.0*dphi**2 - 10.0*ddphi**2])
-
-# pid...
 def critic(info):
     phi = info[0]
     dphi = info[0] - info[1]
-    iphi = np.mean(info)
-    return np.max([0.0, 1.0 - 1.0*phi**2 - 1.0*dphi**2 - 1.0*iphi**2])
+    aux = info[1] - info[2]
+    ddphi = aux - dphi
+    return np.max([0.0, 1.0 - 1.0*(phi**2 + 10*dphi**2 + 10*ddphi**2)])
 
-nn = NeuralNetwork.RL(4)
-past = np.array([0.0])
+def pid(info):
+    phi = info[0]
+    dphi = info[0] - info[1]
+    iphi = np.mean(info)
+#    return 10.0*np.array([phi, dphi, iphi])
+    return 10.0*np.array([phi, dphi, iphi])
+
+nn = NeuralNetwork.RL(3)
 
 timeperf = []
 env = gym.make('CartPole-v0')
-for i_episode in range(80):
+for i_episode in range(100):
     observation = env.reset()
-    obsang = [observation[2], 0.0, 0.0, 0.0]
+    obsang = [observation[2], 0.0, 0.0]
     end = False
-    toptime = 100
+    toptime = 200
+    past = np.array([0.0])
     for t in range(toptime):
         env.render()
         #action = env.action_space.sample()
-        #print(observation)
-        pred = NeuralNetwork.RL_Predict(nn, observation)
+        pred = NeuralNetwork.RL_Predict(nn, pid(obsang))
         action = int(NeuralNetwork.RL_Action(pred))
         past = NeuralNetwork.RL_AvgAct(past, pred[-1])
         observation, reward, done, info = env.step(action)
@@ -85,7 +70,7 @@ for i_episode in range(80):
         r = critic(obsang)
         print(r)
         NeuralNetwork.RL_ARP(nn, pred, past, r, 5.0)
-        if done:
+        if r < 0.6:
             end = True
             timeperf.append(t)
             print("Episode finished after {} timesteps".format(t+1))
